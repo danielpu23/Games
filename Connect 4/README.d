1167f512c114419c55dee9d98b8dfae9c0826b7f
danielpu23 <70337232+danielpu23@users.noreply.github.com>
2021-09-21 00:13:48 -0700

    Update README.d

--- Connect 4/README.d
+++ Connect 4/README.d
@@ -1,1 +1,2 @@
-Connect 4 Game (Work in Progress)
+Connect 4 Game where you play against a simple AI. 
+You will input the appropriate column to drop discs in, and you take turns with the AI until one of you gets 4 in a row (horizontally, vertically, or diagonally)

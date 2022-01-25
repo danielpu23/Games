5048c3123da5b861717abaa66bf994a7c24894a5
danielpu23 <70337232+danielpu23@users.noreply.github.com>
2022-01-24 18:39:20 -0800

    Create readme.d

---
+++ snake/readme.d
@@ -0,0 +1,4 @@
+Simple Snake game in Python using curses API to display a text-based game. 
+The snake is represented by '#' characters and grows in length when it eats apples (represented by '*' characters)
+Score increases each time time the snake eats an apple along with its size, and the game ends when the snake hits 
+itself or goes out of bounds on the screen.
